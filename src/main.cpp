512df7813b7d423e034dc06265375ee5f6018ca0
aviramlachmani <aviraml@post.bgu.ac.il>
2020-10-31 18:41:37 +0200

    aviram update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "Session.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main(int argc, char** argv){
+    if(argc != 2){
+        cout << "usage cTrace <config_path>" << endl;
+        return 0;
+    }
+    Session sess(argv[1]);
+    sess.simulate();
     return 0;
 }
