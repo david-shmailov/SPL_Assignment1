bc1ea4d371384db83769e2c01c6e70520c0427c2
David Shmailov <shmailov@post.bgu.ac.il>
2020-10-31 17:05:52 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/SPL_Assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPL_Assignment1.dir/main.cpp.o"
+  "SPL_Assignment1.pdb"
+  "SPL_Assignment1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SPL_Assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
