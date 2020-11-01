28d4ee81183909b9b5975910f2029070184cd464
David Shmailov <shmailov@post.bgu.ac.il>
2020-11-01 18:27:08 +0200

    updated Graph.cpp

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -4,10 +4,18 @@
 
 #include "../headers/Graph.h"
 #include <vector>
+// TODO test this
+Graph::Graph(std::vector<std::vector<int>> matrix):edges(matrix) {
+    //edges = matrix;
+    infected = std::vector<bool> (edges.size(),false);
+}
 
-Graph::Graph(std::vector<std::vector<int>> matrix) {
-    edges = matrix;
+bool Graph::isInfected(int nodeInd) {
+    return this->infected[nodeInd];
+}
 
+void Graph::infectNode(int nodeInd) {
+    this->infected[nodeInd] = true;
 }
 
 
