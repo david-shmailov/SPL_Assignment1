e7e84c2410b37687606050b08a84914b5ee81e6d
aviramlachmani <aviraml@post.bgu.ac.il>
2020-11-01 21:09:47 +0200

    aviram update

--- src/Session.cpp
+++ src/Session.cpp
@@ -3,8 +3,31 @@
 //
 
 #include "../headers/Session.h"
-void Session::Session(const std::string &path) {}
+//void Session::Session(const std::string &path) {}//TODO
+//void Session::simulate() {}// TODO
+void Session::enqueueInfected(int i) {infected.enqueue(i);}
+int Session::dequeueInfected() {
+    int i= infected.peek();
+    infected.dequeue();
+    return i;}
 std::vector<Agent *> Session::getAgent() const{ return  agents;}
-void Session::addAgent(const Agent &agent) {agents.push_back(agent);}
+TreeType Session::getTreeType() {return treeType;}
+void Session::addAgent(const Agent &agent) {agents.push_back(agent);}//Problem
 void Session::setGraph(const Graph &graph) {g=graph;}
+
+
+
+queue::queue() {i=new std::vector<int>;};
+bool queue::isEmpty() {i->empty();};
+void queue::enqueue(int n){i->push_back(n);};
+int queue::peek() {(*i)[0];}
+int queue::dequeue() {
+    for (int j=0;j<i->size()-1;j++)
+        (*i)[j]=(*i)[j+1];
+    i->pop_back();
+
+}
+
+
+
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "../headers/Session.h"
-
+#include "../src/Session.cpp"
 using namespace std;
 
 int main(int argc, char** argv){
+
+
     if(argc != 2){
-        cout << "usage cTrace <config_path>" << endl;
+        //cout << "usage cTrace <config_path>" << endl;
         return 0;
     }
     //Session sess(argv[1]);
