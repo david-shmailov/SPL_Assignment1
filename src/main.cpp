4fced5a37f5cc36ed7de29aed7dd54d8f827d2e4
aviramlachmani <aviraml@post.bgu.ac.il>
2020-11-01 21:10:03 +0200

    aviram update

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv){
 
 
     if(argc != 2){
-        //cout << "usage cTrace <config_path>" << endl;
+        cout << "usage cTrace <config_path>" << endl;
         return 0;
     }
     //Session sess(argv[1]);
