9f966818fc7d70d9b825a867bc356fc688071d02
David Shmailov <shmailov@post.bgu.ac.il>
2020-11-03 14:44:58 +0200

    changes to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include "../headers/Session.h"
+#include "../headers/json.hpp"
 
 using namespace std;
+using namespace nlohmann;
+using json = nlohmann::json;
 
 int main(int argc, char** argv){
 
     if(argc != 2){
         cout << "usage cTrace <config_path>" << endl;
+
+        ifstream ifs("../config1.json");
+        json jf = json::parse(ifs);
+
         return 0;
     }
     //Session sess(argv[1]);
