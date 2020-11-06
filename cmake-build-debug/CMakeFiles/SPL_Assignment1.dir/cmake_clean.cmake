d4f423899c42ed1ec8f33ac3725b7f504c5fa80f
aviramlachmani <aviraml@post.bgu.ac.il>
2020-11-06 18:52:02 +0200

    aviram update

---
+++ cmake-build-debug/CMakeFiles/SPL_Assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPL_Assignment1.dir/src/Agent.cpp.o"
+  "CMakeFiles/SPL_Assignment1.dir/src/Graph.cpp.o"
+  "CMakeFiles/SPL_Assignment1.dir/src/Session.cpp.o"
+  "CMakeFiles/SPL_Assignment1.dir/src/Tree.cpp.o"
+  "CMakeFiles/SPL_Assignment1.dir/src/main.cpp.o"
+  "SPL_Assignment1"
+  "SPL_Assignment1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SPL_Assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
